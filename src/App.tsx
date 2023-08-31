f8b7d6c66523b1b5d5cde7504cee8f610830410e
CMOISDEAD <camilodavina@gmail.com>
2023-08-31 08:45:35 -0500

    feat: DaisyUI implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="container mx-auto bg-zinc-800">
+    <div className="container mx-auto">
       <h1>Online Library</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
